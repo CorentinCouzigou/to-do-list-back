8d694df655db2fd447655f5428c81742e88c38f3
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-08-22 10:29:01 +0200

    readme in back check

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 // librairies
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 
-// recipes data
+// list des tasks de la to do list
 const tasksList = require('./list.json');
 
-// vars
+//utilisation de express pour l'application et description du port dans notre cas localhost 3001
 const app = express();
 const port = 3001;
 
 
-/* Middlewares */
-// parse request body
-app.use(bodyParser.json());
+// Body parser pour récupérer les envoies de l'utilisateur, mais pas utile pour cette application sans BDD.
+// app.use(bodyParser.json());
 
-// cors
+// Gestion des cors pour permettre au front de faire des requetes
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 
-  // response to preflight request
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
   }
@@ -30,16 +28,16 @@ app.use((req, res, next) => {
   }
 });
 
-
 /* Routes */
 // Page d'accueil du serveur : GET /
-
 app.get('/', (req, res) => {
   console.log('>> GET /');
-  console.log(tasksList);
+  res.sendFile( __dirname + '/index.html');
+});
+app.get('/', (req, res) => {
+  console.log('>> GET /');
   res.json(tasksList);
 });
-
 
 /*
  * Server
